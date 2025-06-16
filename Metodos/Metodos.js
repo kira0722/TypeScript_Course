58da65211557514a142ffa0b998231d871ffb612
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-06-16 10:29:50 -0500

    subiendo metodos con ts

---
+++ Metodos/Metodos.js
@@ -0,0 +1,9 @@
+function printMessage(message) {
+    console.log(message);
+}
+printMessage("hi, 'em message");
+function sum(number1, number2) {
+    return number1 + number2;
+}
+var result = sum(2, 4);
+console.log("the result of the sum its: ".concat(result));
---
+++ Metodos/Metodos.ts
@@ -0,0 +1,12 @@
+function printMessage(message : string): void {
+    console.log(message);
+}
+
+printMessage("hi, 'em message")
+
+function sum (number1 : number, number2 : number): number {
+    return number1 + number2
+}
+let result : number = sum(2, 4)
+
+console.log(`the result of the sum its: ${result}`);
