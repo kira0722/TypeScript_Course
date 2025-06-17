1c21c678573d2adce53137f22df8a0c9a543c9a7
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-06-17 08:59:43 -0500

    subiendo funciones extendidas con generics

---
+++ Extends/Main.ts
@@ -0,0 +1,15 @@
+let nombre1: string = "carlosh"
+
+console.log(nombre1.length);
+
+function obtenerLength <T extends { length: number}> (obj: T): number {
+    let longitud : number = obj.length;
+    let tipo: string = typeof obj;
+    console.log(`el tipo de dato es ${tipo} y la longitud es : ${longitud}`);
+    return obj.length
+}
+
+obtenerLength("hola")
+obtenerLength([1, 2, 3, 4, 5])
+
+console.log(obtenerLength(42));
