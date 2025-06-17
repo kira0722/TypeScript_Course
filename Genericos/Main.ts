c87312c20b3b975996f4a25639aa0b6c62704d36
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-06-17 08:20:38 -0500

    subiendo generics

---
+++ Genericos/Main.ts
@@ -0,0 +1,33 @@
+function identity<T>(arg: T): T {
+    return arg
+}
+
+let output1 = identity<string>("Hello")
+console.log("output1: ",output1);
+
+let output2 = identity<number>(43)
+console.log("output2: ",output2);
+
+
+class Caja<T> {
+    private contenido: T
+
+    constructor(contenido: T) {
+        this.contenido = contenido
+    }
+
+    public obtenerContenido(): T {
+        return this.contenido
+    }
+}
+
+let cajaString = new Caja<string>("Libros")
+console.log("Contenido de la caja de string: ", cajaString.obtenerContenido());
+
+let cajaBoolean = new Caja<boolean>(true)
+console.log("contenido boolean; ", cajaBoolean.obtenerContenido());
+
+let cajaNumber = new Caja<number>(21)
+console.log("caja number: ", cajaNumber.obtenerContenido());
+
+let objeto1 = []
